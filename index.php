fb6b4e09ca6b8db4d013d2a5062f97088c550c9a
rinat <rinat@mail.com>
2018-09-20 12:49:38 +0500

    fix

--- index.php
+++ index.php
@@ -7,13 +7,6 @@ class DbTCsv
     private $password = '';
     private $dbname = 'lady_charm_old';
 
-    public static function main()
-    {
-       // self::getProductsCategoty(44);
-        self::categoryPath(44);
-        self::getCategory();
-    }
-
     private function dbconnect()
     {
         $db = new mysqli($this->host, $this->user, $this->password, $this->dbname) or die ($db->connect_error);
@@ -71,7 +64,7 @@ class DbTCsv
     {
         $class = new DbTCsv();
         $db = $class->dbconnect();
-        $query = "SELECT name FROM taxonomy_term_data WHERE tid =" . $id;
+        $query = "SELECT name FROM taxonomy_term_data WHERE tid =".$id;
         $result = $db->query($query);
         $row = $result->fetch_assoc();
         return $row['name'];
@@ -121,9 +114,9 @@ class DbTCsv
             if ($row['parent'] != 0) {
                 $sectionName = self::getNameCategory($row['parent']);
                 if (array_key_exists($sectionName, $section)) {
-                    $section[$sectionName] += [ $row['tid'] => self::getNameCategory($row['tid'])];
+                    $section[$sectionName] += [$row['tid'] => self::getNameCategory($row['tid'])];
                 } else {
-                    $section[$sectionName] = [ $row['parent'] => self::getNameCategory($row['tid'])];
+                    $section[$sectionName] = [$row['parent'] => self::getNameCategory($row['tid'])];
                 }
             }
         }
@@ -131,102 +124,196 @@ class DbTCsv
         print_r($section);
     }
 
+    public static function category()
+    {
+        $class = new DbTCsv();
+        $db = $class->dbconnect();
+        $ids = [];
+        $query = "SELECT entity_id,taxonomy_catalog_tid from field_revision_taxonomy_catalog";
+        $result = $db->query($query);
+        while ($row = $result->fetch_assoc()) {
+            if ($row['taxonomy_catalog_tid'] == 299) continue;
+            $ids[$row['entity_id']] = $row['taxonomy_catalog_tid'];
+        }
+       // $ids = array_unique($ids);
+        /* foreach ($ids as $id){
+                    $mass[$id] = self::getNameCategory($id);
+                }*/
+        return $ids;
+    }
+
+
     public static function getAllProducts()
     {
         $arr = [];
         $class = new DbTCsv();
         $db = $class->dbconnect();
-        $query = "SELECT vid,sell_price FROM uc_products";
+        $ids = [];
+        $query = "SELECT vid FROM uc_products";
         $result = $db->query($query);
-        while ($row = $result->fetch_array(MYSQLI_BOTH)) {
-            $arr[$row[0]] = ['id' => $row[0]];
-            $arr[$row[0]] += ['sell_price' => $row[1]];
+        while ($row = $result->fetch_assoc()) {
+            $ids[] = $row['vid'];
         }
-        foreach ($arr as $row) {
-            $query = "SELECT bundle,body_value from field_data_body WHERE entity_id = " . $row['id']." LIMIT 10";
-            $result = $db->query($query);
-            $result = $result->fetch_assoc();
-            $names = self::getName($row["id"]);
-            $arr[$row["id"]] += ['type' => $names['type']];
-            $arr[$row["id"]] += ['name' => $names['title']];
-            $arr[$row["id"]] += ['article' => $result['bundle'].$row["id"]];
-            $arr[$row["id"]] += ['description' => $result['body_value']];
-            $arr[$row["id"]] += ['brand' => self::getBrand($row["id"])];
-            $arr[$row["id"]] += ['images' => self::getImage($row["id"])];
-            $arr[$row["id"]] += ['category' => self::getSection($row["id"])];
+        foreach ($ids as $id) {
+            $catid = self::category();
+            if(empty($catid[$id])) continue;
+            $arr[$id] = ['IE_XML_ID' => $id];
+            $arr[$id] += ['IE_NAME' => self::getName($id)];
+            $arr[$id] += ['IE_ACTIVE' => 'Y'];
+            $arr[$id] += ['IE_PREVIEW_TEXT' => self::getDescription($id)];
+            $arr[$id] += ['IE_PREVIEW_PICTURE' => self::getImage($id)];
+            $arr[$id] += ['IE_DETAIL_TEXT' => ' '];
+            $arr[$id] += ['IP_PROP15' => self::getNameCategory($catid[$id]) . $id];
+            $arr[$id] += ['IP_PROP16' => self::getBrand($id)];
+            $arr[$id] += ['IE_CODE' => self::getName($id) . $id];
+            $arr[$id] += ['IC_GROUP0' => self::categoryPath($catid[$id])[0]];
+            $arr[$id] += ['IC_GROUP1' => self::categoryPath($catid[$id])[1]];
+            $arr[$id] += ['IC_GROUP2' => ' '];
+            $arr[$id] += ['CP_QUANTITY' => 0];
+            $arr[$id] += ['CV_PRICE_1' => self::getPrice($id)];
+            $arr[$id] += ['CV_CURRENCY_1' => 'RUB'];
         }
-        echo "<pre>";
-        print_r($arr);
+            return $arr;
     }
-    public static function getPrice($id){
+
+    public static function getPrice($id)
+    {
         $class = new DbTCsv();
         $db = $class->dbconnect();
-        $query = "SELECT sell_price FROM uc_products WHERE vid = ".$id;
+        $query = "SELECT sell_price FROM uc_products WHERE vid = " . $id;
         $result = $db->query($query);
         $row = $result->fetch_assoc();
         return $row['sell_price'];
     }
 
-    public static function getDescription($id){
+    public static function getDescription($id)
+    {
         $class = new DbTCsv();
         $db = $class->dbconnect();
-        $query = "SELECT body_value from field_data_body WHERE entity_id = " .$id;
+        $query = "SELECT body_value from field_data_body WHERE entity_id = " . $id;
         $result = $db->query($query);
         $row = $result->fetch_assoc();
         return $row['body_value'];
     }
 
 
-    public static function categoryPath($id){
+    public static function categoryPath($id)
+    {
         $path = [
-          44 =>    ['Бижутерия','Серьги'],
-          101 =>   ['Бижутерия','Серьги'],
-          45 =>    ['Бижутерия','Кольца'],
-          100 =>   ['Бижутерия','Кольца'],
-          46 =>    ['Бижутерия','Колье и подвески'],
-          99 =>    ['Бижутерия','Колье и подвески'],
-          47 =>    ['Бижутерия','Браслеты'],
-          97 =>    ['Бижутерия','Браслеты'],
-          108 =>   ['Бижутерия','Гарнитуры'],
-          116 =>   ['Бижутерия','Гарнитуры'],
+            // Ювелирная бижутерия
+            37 => ['Бижутерия', 'Броши'],
+            44 => ['Бижутерия', 'Серьги'],
+            45 => ['Бижутерия', 'Кольца'],
+            46 => ['Бижутерия', 'Колье и подвески'],
+            47 => ['Бижутерия', 'Браслеты'],
+            108 => ['Бижутерия', 'Гарнитуры'],
+            // Дизайнерская бижутерия
+            100 => ['Бижутерия', 'Кольца'],
+            99 => ['Бижутерия', 'Колье и подвески'],
+            101 => ['Бижутерия', 'Серьги'],
+            103 => ['Бижутерия', 'Серьги люстры, свадебные украшения'],
+            98 => ['Бижутерия', 'Браслеты'],
+            116 => ['Бижутерия', 'Гарнитуры'],
+            102 => ['Бижутерия', 'Броши'],
+            //  Обувь осень-зима
+            355 => ['Обувь', 'Сапоги'],
+            357 => ['Обувь', 'Ботфорты'],
+            358 => ['Обувь', 'Ботинки'],
+            359 => ['Обувь', 'Ботильоны'],
+            360 => ['Обувь', 'Высокие кеды'],
+            361 => ['Обувь', 'Полуботинки'],
+            362 => ['Обувь', 'Полусапоги'],
+            363 => ['Обувь', 'Кроссовки'],
+            364 => ['Обувь', 'Сникерсы'],
+            //  Обувь весна-лето
+            339 => ['Обувь', 'Кроссовки'],
+            366 => ['Обувь', 'Туфли'],
+            367 => ['Обувь', 'Шлепки'],
+            368 => ['Обувь', 'Сникерсы'],
+            369 => ['Обувь', 'Кеды'],
+            370 => ['Обувь', 'Ботильоны'],
+            371 => ['Обувь', 'Лоферы'],
+            372 => ['Обувь', 'Босоножки'],
+            373 => ['Обувь', 'Эспадрильи'],
+            //  Мужские товары
+            402 => ['Мужские товары', 'Украшения'],
+            403 => ['Мужские товары', 'Сумки и Рюкзаки'],
+            404 => ['Мужские товары', 'Кошельки, обложки, ключницы'],
+            405 => ['Мужские товары', 'Мужская обувь'],
+            406 => ['Мужские товары', 'Перчатки'],
+            407 => ['Мужские товары', 'Часы'],
+            408 => ['Мужские товары', 'Платки палантины шарфы'],
+            409 => ['Мужские товары', 'Мужские солнцезащитные очки'],
+            410 => ['Мужские товары', 'Ремни'],
+            // Украшения для волос
+            380 => ['Бижутерия', 'Украшения для волос'],
+            382 => ['Бижутерия', 'Украшения для волос'],
+            383 => ['Бижутерия', 'Украшения для волос'],
+            426 => ['Бижутерия', 'Украшения для волос'],
+            //  Самая красивая
+            // 205 =>  ['Самая красивая', ''],
+            250 => ['Часы', ''],
+            289 => ['Платки палантины шарфы', ''],
+            //  Сумки и Рюкзаки
+            295 => ['Сумки и Рюкзаки', 'Сумки'],
+            379 => ['Сумки и Рюкзаки', 'Сумки люкс'],
+            427 => ['Сумки и Рюкзаки', 'Рюкзаки'],
+            //  Аксессуары
+            301 => ['Аксессуары', 'Брелки'],
+            307 => ['Аксессуары', 'Очки'],
+            338 => ['Аксессуары', 'Ремни'],
+            // Другое
+            300 => ['Кошельки, Ключницы, Обложки', ''],
+            418 => ['Шапки, Кепки, Шляпы', ''],
+            /**
+             *
+             * [259] => Украшения
+             * [381] => Заколки
+             * [382] => Крабики
+             * [383] => Резинки
+             *
+             */
         ];
-         return $path[$id];
+        return $path[$id];
     }
 
-    public static function branToMachine (){
+    public static function branToMachine()
+    {
         return;
     }
 
-    public static function getProductsCategoty($catid){
+    public static function getProductsCategoty($catid)
+    {
         $class = new DbTCsv();
         $db = $class->dbconnect();
         $ids = [];
         $arr = [];
-        $query = "SELECT entity_id from field_revision_taxonomy_catalog WHERE taxonomy_catalog_tid = ".$catid;
+        $query = "SELECT entity_id from field_revision_taxonomy_catalog WHERE taxonomy_catalog_tid = " . $catid;
         $result = $db->query($query);
-        while($row = $result->fetch_assoc()){
+        while ($row = $result->fetch_assoc()) {
             $ids[] = $row['entity_id'];
         }
         foreach ($ids as $id) {
-            $arr[$id] =    ['IE_XML_ID' => $id];
-            $arr[$id] +=   ['IE_NAME' => self::getName($id)];
-            $arr[$id] +=   ['IE_ACTIVE' => 'Y'];
-            $arr[$id] +=   ['IE_PREVIEW_TEXT' => self::getDescription($id)];
-            $arr[$id] +=   ['IE_PREVIEW_PICTURE' => self::getImage($id)];
-            $arr[$id] +=   ['IE_DETAIL_TEXT' => ' '];
-            $arr[$id] +=   ['IP_PROP15' => self::getNameCategory($catid).$id];
-            $arr[$id] +=   ['IP_PROP16' => self::getBrand($id)];
-            $arr[$id] +=   ['IE_CODE' => self::getName($id).$id];
-            $arr[$id] +=   ['IC_GROUP0' =>self::categoryPath($catid)[0]];
-            $arr[$id] +=   ['IC_GROUP1' =>self::categoryPath($catid)[1]];
-            $arr[$id] +=   ['IC_GROUP2' =>' '];
-            $arr[$id] +=   ['CP_QUANTITY' => 0];
-            $arr[$id] +=   ['CV_PRICE_1' => self::getPrice($id)];
-            $arr[$id] +=   ['CV_CURRENCY_1' => 'RUB'];
+            $arr[$id] = ['IE_XML_ID' => $id];
+            $arr[$id] += ['IE_NAME' => self::getName($id)];
+            $arr[$id] += ['IE_ACTIVE' => 'Y'];
+            $arr[$id] += ['IE_PREVIEW_TEXT' => self::getDescription($id)];
+            $arr[$id] += ['IE_PREVIEW_PICTURE' => self::getImage($id)];
+            $arr[$id] += ['IE_DETAIL_TEXT' => ' '];
+            $arr[$id] += ['IP_PROP15' => self::getNameCategory($catid) . $id];
+            $arr[$id] += ['IP_PROP16' => self::getBrand($id)];
+            $arr[$id] += ['IE_CODE' => self::getName($id) . $id];
+            $arr[$id] += ['IC_GROUP0' => self::categoryPath($catid)[0]];
+            $arr[$id] += ['IC_GROUP1' => self::categoryPath($catid)[1]];
+            $arr[$id] += ['IC_GROUP2' => ' '];
+            $arr[$id] += ['CP_QUANTITY' => 0];
+            $arr[$id] += ['CV_PRICE_1' => self::getPrice($id)];
+            $arr[$id] += ['CV_CURRENCY_1' => 'RUB'];
         }
         return $arr;
     }
 }
+
 require_once 'RecordCsv.php';
 $record = new RecordCsv();
-$record->record(DbTCsv::getProductsCategoty(45));
+$record->record(DbTCsv::getAllProducts());
