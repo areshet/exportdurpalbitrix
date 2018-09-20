1e4fe0fcfdeb09805d9501ec0c46787611a5b35c
rinat <rinat@mail.com>
2018-09-20 17:45:11 +0500

    fix method

--- RecordCsv.php
+++ RecordCsv.php
@@ -2,18 +2,40 @@
 
 class RecordCsv
 {
-    private $path = '/import3.csv';
-    const HEAD = ['IE_XML_ID','IE_NAME','IE_ACTIVE','IE_PREVIEW_TEXT','IE_PREVIEW_PICTURE','IE_DETAIL_TEXT','IP_PROP15','IP_PROP16','IE_CODE','IC_GROUP0','IC_GROUP1','IC_GROUP2','CP_QUANTITY','CV_PRICE_1','CV_CURRENCY_1'];
+    private $path = 'importAll.csv';
+    const HEAD = ['IE_XML_ID', 'IE_NAME', 'IE_ACTIVE', 'IE_PREVIEW_TEXT', 'IE_PREVIEW_PICTURE', 'IE_DETAIL_TEXT','IE_DETAIL_PICTURE', 'IP_PROP15', 'IP_PROP16', 'IE_CODE', 'IC_GROUP0', 'IC_GROUP1', 'IC_GROUP2', 'CP_QUANTITY', 'CV_PRICE_1', 'CV_CURRENCY_1'];
 
-    public function record($category){
+    public function record($category)
+    {
         $i = 0;
         $fp = fopen($this->path, 'w');
-        fputcsv($fp,self::HEAD,';');
+        fputcsv($fp, self::HEAD, ';');
         foreach ($category as $row) {
-            if(fputcsv($fp, $row,';')){ $i++;   }  else {   continue;   }
+            if (fputcsv($fp, $row, ';')) {
+                $i++;
+            } else {
+                continue;
+            }
         }
         fclose($fp);
-        echo "Added ".$i." lines";
+        echo "Added " . $i . " lines";
+    }
+
+    public function recordCategoty($category, $name)
+    {
+        $i = 0;
+        $path = '.\import_' . $name . '.csv';
+        $fp = fopen($path, 'w');
+        fputcsv($fp, self::HEAD, ';');
+        foreach ($category as $row) {
+            if (fputcsv($fp, $row, ';')) {
+                $i++;
+            } else {
+                continue;
+            }
+        }
+        fclose($fp);
+        echo "Added " . $i . " lines";
     }
 }
 
